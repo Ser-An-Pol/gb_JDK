6f52a6fa1202bca28c876a656e8c3402f42a43d3
Ser-An-Pol <seranpol@icloud.com>
2024-11-14 23:22:18 +0300

    homework5/ => Задача о философах

---
+++ homework5/src/Philosopher.java
@@ -0,0 +1,58 @@
+import java.util.Random;
+
+public class Philosopher extends Thread {
+    private final int ID;
+    private final Status[] states;
+    private final int limitMeals;
+    private int countMeals;
+    private final Random random = new Random();
+
+    public Philosopher(int ID, int limitMeals, Status[] states) {
+        this.ID = ID;
+        this.limitMeals = limitMeals;
+        this.states = states;
+        countMeals = 0;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        while (countMeals < limitMeals) {
+            try {
+                Thread.sleep(random.nextInt(3000));
+                tryChangeStatus();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        states[ID] = Status.THINKING;
+        System.out.println(ID + "-й пошел домой...");
+    }
+
+    private synchronized void tryChangeStatus() {
+        if (states[ID] == Status.EATING) {
+            states[ID] = Status.THINKING;
+            System.out.println(ID + "-й думать будет...");
+        }
+        else {
+            if (nextState() == Status.THINKING && prevState() == Status.THINKING) {
+                states[ID] = Status.EATING;
+                countMeals++;
+                System.out.println(ID + "-й будет кушать " + countMeals + "-й раз...");
+            }
+            else {
+                System.out.println(ID + "-й ещё подумает немного...");
+            }
+        }
+    }
+
+    private Status nextState() {
+        int next = (ID == states.length - 1) ? 0 : ID + 1;
+        return states[next];
+    }
+
+    private Status prevState() {
+        int prev = (ID == 0) ? states.length - 1 : ID - 1;
+        return states[prev];
+    }
+}
---
+++ homework5/src/Table.java
@@ -0,0 +1,44 @@
+public class Table extends Thread {
+    private final int countOfPhilosopher;
+    private final int limitMeals;
+    private volatile Status[] tableStatus;
+    private final Philosopher[] philosophers;
+
+    public Table(int countOfPhilosopher, int limitMeals) {
+        this.countOfPhilosopher = countOfPhilosopher;
+        this.limitMeals = limitMeals;
+        this.tableStatus = new Status[countOfPhilosopher];
+        initStatuses();
+        this.philosophers = new Philosopher[countOfPhilosopher];
+        initPhilosophers();
+    }
+
+    private void initStatuses() {
+        for (int i = 0; i < countOfPhilosopher; i++) {
+            tableStatus[i] = Status.THINKING;
+        }
+    }
+
+    private void initPhilosophers() {
+        for (int i = 0; i < countOfPhilosopher; i++) {
+            philosophers[i] = new Philosopher(i, limitMeals, tableStatus);
+        }
+    }
+
+    private boolean isAllFinish() {
+        for (int i = 0; i < countOfPhilosopher; i++) {
+            if (philosophers[i].getState() != State.TERMINATED) return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        for (int i = 0; i < countOfPhilosopher; i++) {
+            philosophers[i].start();
+        }
+        while (!isAllFinish());
+        System.out.println("Все разошлись. Доброй ночи!");
+    }
+}
