05dc796bba7caf854bc45b89c5bebd387e24a141
Ser-An-Pol <seranpol@icloud.com>
2024-11-16 16:45:35 +0300

    seminar6_2/ => Gradle project

--- seminar6_2/src/main/java/ru/seranpol/Main.java
+++ seminar6_2/src/main/java/ru/seranpol/Main.java
@@ -1,6 +1,9 @@
 package ru.seranpol;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+import java.util.*;
 
 /**
  * Создать свой Java Gradle проект;
@@ -16,6 +19,23 @@ import com.google.common.collect.Lists;
  */
 public class Main {
     public static void main(String[] args) {
-        Lists
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2 ,3, 4, 5, 6, 7));
+
+        System.out.println(list);
+        System.out.println(Lists.reverse(list));
+
+        String string = "Hello, world!";
+        System.out.println(Lists.charactersOf(string));
+
+        Lists.partition(list, 2).forEach(System.out::println);
+
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2 ,3, 4, 5, 6, 7));
+        Set<Integer> set2 = new HashSet<>(Arrays.asList(5, 6, 7, 8, 9, 10, 11));
+        System.out.println("set1: " + set1);
+        System.out.println("set2: " + set2);
+        System.out.println(Sets.union(set1, set2));
+        System.out.println(Sets.intersection(set1, set2));
+        System.out.println(Sets.symmetricDifference(set1, set2));
+
     }
 }
